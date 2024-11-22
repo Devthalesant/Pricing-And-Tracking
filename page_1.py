907151d90159fb85a84f0ffed9aca7514447bf29
Devthalesant <tha.santoro2000@gmail.com>
2024-11-22 13:05:51 +0000

    Updating files

--- page_1.py
+++ page_1.py
@@ -1,14 +1,13 @@
 import streamlit as st
 import pandas as pd
-import datetime
-from streamlit_gsheets import GSheetsConnection
 
-st.set_page_config(page_title="Teste DataFrame", page_icon="💎",layout="wide")
+st.set_page_config(page_title="Teste DataFrame", page_icon="💎", layout="wide")
 
 @st.cache_data
+def load_data():
+    return pd.read_csv("/content/drive/MyDrive/python - Thales/P&T/Bases CSV/ALL/data_sales_2024_updated_try.csv")
 
-df_sales = pd.read_csv("/content/drive/MyDrive/python - Thales/P&T/Bases CSV/ALL/data_sales_2024_updated_try.csv")
-
+df_sales = load_data()
 
 st.title("Meu novo dataframe")
 
