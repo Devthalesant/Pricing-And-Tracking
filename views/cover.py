0be3004add5e234348d01a609ceee28d5ad53862
Devthalesant <tha.santoro2000@gmail.com>
2024-11-25 21:27:27 +0000

    Updating files

--- views/cover.py
+++ views/cover.py
@@ -4,3 +4,6 @@ import pandas as pd
 st.set_page_config(page_title="DashBoard Financeiro - Pró-Corpo Estética", page_icon="💎", layout="wide")
 
 st.title("DashBoard Financeiro - Pró-Corpo Estética")
+
+st.sidebar.title("Navigation")
+page = st.sidebar.radio("Go to", ["Cover", "Sales View", "Appointments View", "Merged View"])
