fbf01a5a25dd023e33064ac375e64bc8bdc631d0
Devthalesant <tha.santoro2000@gmail.com>
2024-11-25 21:28:06 +0000

    Updating files

--- views/cover.py
+++ views/cover.py
@@ -5,5 +5,5 @@ st.set_page_config(page_title="DashBoard Financeiro - Pró-Corpo Estética", pag
 
 st.title("DashBoard Financeiro - Pró-Corpo Estética")
 
-st.sidebar.title("Navigation")
+st.sidebar.title("Análises - Pró-Corpo")
 page = st.sidebar.radio("Go to", ["Cover", "Sales View", "Appointments View", "Merged View"])
