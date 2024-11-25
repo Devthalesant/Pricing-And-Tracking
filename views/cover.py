55b1884fb8f99d38dc88507a0610a69a072cbccc
Devthalesant <tha.santoro2000@gmail.com>
2024-11-25 21:26:07 +0000

    Updating files

--- views/cover.py
+++ views/cover.py
@@ -3,3 +3,4 @@ import pandas as pd
 
 st.set_page_config(page_title="DashBoard Financeiro - Pró-Corpo Estética", page_icon="💎", layout="wide")
 
+st.title("DashBoard Financeiro - Pró-Corpo Estética")
