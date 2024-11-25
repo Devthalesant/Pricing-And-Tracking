3204f700a774ecb2d7b3aec96eff3da9b2c9142f
Devthalesant <tha.santoro2000@gmail.com>
2024-11-25 21:37:32 +0000

    Updating files

--- app.py
+++ app.py
@@ -1,27 +1,44 @@
 import streamlit as st
+import pandas as pd
 
 # --- PAGE SETUP ---
-sample_page = st.Page(
+cover_page = st.Page(
     "views/cover.py",
-    title="Sample Page 1",
-    icon=":material/thumb_up:",
+    title="Cover",
+    icon="🏠",
+    is_sidebar=True,
+)
+
+sales_page = st.Page(
+    "views/sales_view.py",
+    title="Sales View",
+    icon=":money_with_wings:",
     default=True,
 )
 
-# --- NAVIGATION SETUP [WITHOUT SECTIONS] ---
-# pg = st.navigation(pages=[about_page, project_1_page, project_2_page])
+appointments_page = st.Page(
+    "views/appointments_view.py",
+    title="Appointments View",
+    icon=":calendar:",
+)
 
-# --- NAVIGATION SETUP [WITH SECTIONS]---
+merged_page = st.Page(
+    "views/merged_view.py",
+    title="Merged View",
+    icon=":bar_chart:",
+)
+
+# --- NAVIGATION SETUP WITH SECTIONS ---
 pg = st.navigation(
     {
-        "Pages": [sample_page]
+        "Sales": [sales_page],
+        "Appointments": [appointments_page],
+        "Merged": [merged_page]
     }
 )
 
-
 # --- SHARED ON ALL PAGES ---
 # st.logo("assets/codingisfun_logo.png")
-
 
 # --- RUN NAVIGATION ---
 pg.run()
