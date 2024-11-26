4d5bdd19001129555c33598dea72025d4b6c85f9
Devthalesant <tha.santoro2000@gmail.com>
2024-11-26 21:10:48 +0000

    Updating files

--- views/cover.py
+++ views/cover.py
@@ -15,6 +15,19 @@ page = st.sidebar.radio("Go to", ["Visão - Vendas", "Visão - Agendamentos", "I
 # Image path for the logo
 image_path = "/content/Pricing-And-Tracking/logo_vertical.png"
 
+# Path to All_indicators_sales: 
+df_sales_path_all_indicators = "/content/Pricing-And-Tracking/base_de_dados/all_indicators_sales.csv"
+df_sales_all_indicators = pd.read_csv(df_sales_all_indicators)
+
 # Display the image
 st.image(image_path, caption="Logo", use_column_width=True)
 
+if page == "Visão - Vendas":
+    # Content for the "Visão - Vendas" page
+    st.header("Visão - Vendas")
+    st.dataframe(df_sales_all_indicators)
+
+# elif page == "Visão - Agendamentos":
+#     # Content for the "Visão - Agendamentos" page
+#     st.header("Visão - Agendamentos")
+#     st.dataframe(df_appointmnents)
