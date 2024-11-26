c55aa67b6d2bb70bda19696a4ec902856d451005
Devthalesant <tha.santoro2000@gmail.com>
2024-11-26 20:49:10 +0000

    Updating files

--- views/cover.py
+++ views/cover.py
@@ -1,13 +1,18 @@
 import streamlit as st
 import pandas as pd
 
+# Set up the Streamlit page
 st.set_page_config(page_title="DashBoard Financeiro - Pró-Corpo Estética", page_icon="💎", layout="wide")
 
+# Title of the Dashboard
 st.title("DashBoard Financeiro - Pró-Corpo Estética")
 
+# Sidebar navigation
 st.sidebar.title("Análises - Pró-Corpo")
-page = st.sidebar.radio("Go to", ["Iníco", "Visão - Vendas", "Visão - Agendamentos", "Indicadores Gerais"])
+page = st.sidebar.radio("Go to", ["Início", "Visão - Vendas", "Visão - Agendamentos", "Indicadores Gerais"])
 
+# Image path for the logo
 image_path = "/content/Pricing-And-Tracking/base_de_dados/Logo_vertical.png"
 
+# Display the image
 st.image(image_path, caption="Logo", use_column_width=True)
