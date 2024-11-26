f044dd189fbdf695f413a44956e91d84de59cf40
Devthalesant <tha.santoro2000@gmail.com>
2024-11-26 20:50:34 +0000

    Updating files

--- views/cover.py
+++ views/cover.py
@@ -12,7 +12,7 @@ st.sidebar.title("Análises - Pró-Corpo")
 page = st.sidebar.radio("Go to", ["Início", "Visão - Vendas", "Visão - Agendamentos", "Indicadores Gerais"])
 
 # Image path for the logo
-image_path = "/content/Pricing-And-Tracking/base_de_dados/Logo_vertical.png"
+image_path = "/content/Pricing-And-Tracking/base_de_dados/logo_vertical.png"
 
 # Display the image
 st.image(image_path, caption="Logo", use_column_width=True)
