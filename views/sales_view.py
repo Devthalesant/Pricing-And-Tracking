c0bae1b6653baa69e38b66a50cc92c8720111758
Devthalesant <tha.santoro2000@gmail.com>
2024-12-03 21:31:04 +0000

    Updating files

--- views/sales_view.py
+++ views/sales_view.py
@@ -3,10 +3,10 @@ import pandas as pd
 import os
 import random
 
-Month_list = ["Janeiro","Fevereiro","Março","Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembro"]
+Month_list = ["Anual","Janeiro","Fevereiro","Março","Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembro"]
 
 #Path to the sales indicators CSV file
-df_sales_all_indicators_path = "base_de_dados/sales/Sales_View/all_indicators_sales.csv"
+df_sales_revenue_and_operating_profit_path = "base_de_dados/sales/Sales_View/revenue_and_operating_profit.csv"
 df_sales_average_price_path = "base_de_dados/sales/Sales_View/df_average_price.csv"
 df_sales_contribution_margin_path = "base_de_dados/sales/Sales_View/df_contribution_margin.csv"
 df_sales_main_products_path = "base_de_dados/sales/Sales_View/df_main_products.csv"
@@ -18,29 +18,59 @@ month_selector = st.selectbox("Escolha o mês", Month_list)
 
 
 if sales_option == "Todos Indicadores":
-  df_sales_all_indicators = pd.read_csv(df_sales_all_indicators_path)
-  df_sales_all_indicators = df_sales_all_indicators.loc[df_sales_all_indicators['Mês venda'].isin([month_selector])]
-  st.dataframe(df_sales_all_indicators)
+  df_sales_revenue_and_operating_profit = pd.read_csv(df_sales_revenue_and_operating_profit_path)
+
+  if month_selector == "Anual":
+    df_sales_revenue_and_operating_profit = df_sales_revenue_and_operating_profit
+    st.dataframe(df_sales_revenue_and_operating_profit)
+
+  else:
+    df_sales_revenue_and_operating_profit = df_sales_revenue_and_operating_profit.loc[df_sales_revenue_and_operating_profit['Mês venda'].isin([month_selector])]
+    st.dataframe(df_sales_revenue_and_operating_profit)
 
 elif sales_option == "Preço Médio":
   df_sales_average_price = pd.read_csv(df_sales_average_price_path)
-  df_sales_average_price = df_sales_average_price.loc[df_sales_average_price['Mês venda'].isin([month_selector])]
-  st.dataframe(df_sales_average_price)
+
+  if month_selector == "Anual":
+    df_sales_average_price = df_sales_average_price
+    st.dataframe(df_sales_average_price)
+    
+  else:
+    df_sales_average_price = df_sales_average_price.loc[df_sales_average_price['Mês venda'].isin([month_selector])]
+    st.dataframe(df_sales_average_price)
 
 elif sales_option == "Margem de Contribuição":
   df_sales_contribution_margin = pd.read_csv(df_sales_contribution_margin_path)
-  df_sales_contribution_margin = df_sales_contribution_margin.loc[df_sales_contribution_margin['Mês venda'].isin([month_selector])]
-  st.dataframe(df_sales_contribution_margin)
 
-elif sales_option == "Produtos mais Vendidos":
+  if month_selector == "Anual":
+    df_sales_contribution_margin = df_sales_contribution_margin
+    st.dataframe(df_sales_contribution_margin)
+    
+  else:
+    df_sales_contribution_margin = df_sales_contribution_margin.loc[df_sales_contribution_margin['Mês venda'].isin([month_selector])]
+    st.dataframe(df_sales_contribution_margin)
+
+elif sales_option == "Produtos mais Vendidos"
   df_sales_main_products = pd.read_csv(df_sales_main_products_path)
-  df_sales_main_products = df_sales_main_products.loc[df_sales_main_products['Mês venda'].isin([month_selector])]
-  st.dataframe(df_sales_main_products)
+
+  if month_selector == "Anual":
+    df_sales_main_products = df_sales_main_products
+    st.dataframe(df_sales_main_products)
+    
+  else:
+    df_sales_main_products = df_sales_main_products.loc[df_sales_main_products['Mês venda'].isin([month_selector])]
+    st.dataframe(df_sales_main_products)
 
 elif sales_option == "Quantidade Vendida":
   df_sales_quantity_sold = pd.read_csv(df_sales_quantity_sold_path)
-  df_sales_quantity_sold = df_sales_quantity_sold.loc[df_sales_quantity_sold['Mês venda'].isin([month_selector])]
-  st.dataframe(df_sales_quantity_sold)
+
+  if month_selector == "Anual":
+    df_sales_quantity_sold = df_sales_quantity_sold
+    st.dataframe(df_sales_quantity_sold)
+    
+  else:
+    df_sales_quantity_sold = df_sales_quantity_sold.loc[df_sales_quantity_sold['Mês venda'].isin([month_selector])]
+    st.dataframe(df_sales_quantity_sold)
 
 else:
   st.write("Escolha uma opção válida.")
