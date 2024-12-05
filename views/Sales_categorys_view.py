e4151a69352ee48ee7c9ae64a8195b9ab34c1f03
Devthalesant <tha.santoro2000@gmail.com>
2024-12-05 22:43:19 +0000

    Updating files

--- views/Sales_categorys_view.py
+++ views/Sales_categorys_view.py
@@ -18,7 +18,7 @@ groupby_for_sales = df_sales.groupby(["Unidade", "Mês venda", "Grupo"]).agg({"V
 
 groupby_for_sales_evolution = groupby_for_sales.groupby(["Grupo", "Mês venda"]).agg({"Valor liquido item" : "sum"}).reset_index()
 
-pd.dataframe(groupby_for_sales_evolution)
+st.dataframe(groupby_for_sales_evolution)
 # Evolution Sales 
   #Medicina Estética
 
