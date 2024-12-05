0f3e1a92e503c889080844bf67af1eed45aa3bda
Devthalesant <tha.santoro2000@gmail.com>
2024-12-05 22:02:13 +0000

    Updating files

--- views/Sales_categorys_view.py
+++ views/Sales_categorys_view.py
@@ -7,7 +7,7 @@ df_sales_path = "base_de_dados/sales/Sales_View/Cópia de data_sales_2024_updat
 df_sales = pd.read_csv(df_sales_path)
 
 df_sales = df_sales.loc[df_sales['CORTESIA'] != True]
-groupby_for_sales = df_sales.groupby(["Unidade", "Grupo", "Mês venda", "Grupo"]).agg({"Valor liquido item": "sum","Quantidade" : "sum",
+groupby_for_sales = df_sales.groupby(["Unidade", "Mês venda", "Grupo"]).agg({"Valor liquido item": "sum","Quantidade" : "sum",
                                                                                       "Valor tabela item" : "sum", "Custo Direto" : "sum"}).reset_index()
 
 
