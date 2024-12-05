9a67598f043647128286b660dc9f3e340b86ae05
Devthalesant <tha.santoro2000@gmail.com>
2024-12-05 21:51:22 +0000

    Updating files

--- app.py
+++ app.py
@@ -1,48 +1,34 @@
 import streamlit as st
 
 # --- PAGE SETUP ---
-# Páginas principais
 page_1 = st.Page(
     "views/sales_view.py",
     title="Visão - Vendas",
     icon=":material/thumb_up:"
 )
 
+# --- PAGE SETUP ---
 page_2 = st.Page(
     "views/appointments_view.py",
-    title="Visão - Agendamento",
+    title="VIsão - Agendamento",
     icon=":material/home:",
     default=True,
 )
 
-# --- SUBPAGES SETUP FOR PAGE 1 ---
-subpage_1_1 = st.Page(
-    "views/sales_subpage_1.py",
-    title="Subpágina 1.1",
-)
-subpage_1_2 = st.Page(
-    "views/sales_subpage_2.py",
-    title="Subpágina 1.2",
-)
+# --- NAVIGATION SETUP [WITHOUT SECTIONS] ---
+# pg = st.navigation(pages=[about_page, project_1_page, project_2_page])
 
-# --- SUBPAGES SETUP FOR PAGE 2 ---
-subpage_2_1 = st.Page(
-    "views/appointments_subpage_1.py",
-    title="Subpágina 2.1",
-)
-subpage_2_2 = st.Page(
-    "views/appointments_subpage_2.py",
-    title="Subpágina 2.2",
-)
-
-# --- NAVIGATION SETUP ---
+# --- NAVIGATION SETUP [WITH SECTIONS]---
 pg = st.navigation(
     {
-        "Pages": [page_1, page_2],
-        "Subpáginas - Vendas": [subpage_1_1, subpage_1_2],
-        "Subpáginas - Agendamento": [subpage_2_1, subpage_2_2],
+        "Pages": [page_1,page_2]
     }
 )
+
+
+# --- SHARED ON ALL PAGES ---
+# st.logo("assets/codingisfun_logo.png")
+
 
 # --- RUN NAVIGATION ---
 pg.run()
---
+++ views/Sales_categorys_view.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+
+# Here i wanna show the evolution of Sales Costs of Sales and serve, contribuition margin, ...
+
+df_sales_path = "base_de_dados/sales/Sales_View/Cópia de data_sales_2024_updated.csv"
+df_sales = pd.read_csv(df_sales_path)
+
+
+
+
