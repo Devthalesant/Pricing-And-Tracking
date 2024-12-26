9f9a21ca616dd5abb00ab230871745d4f5f61d27
Devthalesant <tha.santoro2000@gmail.com>
2024-12-26 21:42:59 +0000

    Updating files

--- auxiliar/catching_and_treating_values_functions.py
+++ auxiliar/catching_and_treating_values_functions.py
@@ -23,11 +23,6 @@ def get_dataframe_from_mongodb(collection_name, database_name, query={}):
 
 # Function to extract the informations at the quote_itens
 def extract_quote_items(row):
-
-    # Getting a dataframe of billchaeges
-    billcharges_df = get_dataframe_from_mongodb(collection_name="billcharges_db", database_name="dash_midia")
-    # Dropping dulicates from quote_ID because there is all i need in quote_items:
-    billcharges_df.drop_duplicates(subset=['quote_id'],keep='first', inplace=True)
     items = []
     # Ajustando a regex para capturar corretamente todos os elementos
     for item in row['quote_items'].split(';'):
--- views/general_view.py
+++ views/general_view.py
@@ -3,8 +3,11 @@ from auxiliar.catching_and_treating_values_functions import *
 import streamlit as st
 import pandas as pd
 
-# Chame a função que trata os valores. Certifique-se de que essa função retorna ou manipula new_df de forma correta.
-new_df = treating_values()  # Supondo que treating_values retorna um DataFrame
+# Getting a dataframe of billchaeges
+billcharges_df = get_dataframe_from_mongodb(collection_name="billcharges_db", database_name="dash_midia")
+# Dropping dulicates from quote_ID because there is all i need in quote_items:
+billcharges_df.drop_duplicates(subset=['quote_id'],keep='first', inplace=True)
 
+new_df = treating_values()
 # Exibe o DataFrame no Streamlit
 st.dataframe(new_df)
