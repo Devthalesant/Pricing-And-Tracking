d1e1e24d54dfdc055abf02166295f7296b7bb3d3
Devthalesant <tha.santoro2000@gmail.com>
2024-12-26 20:43:36 +0000

    Updating files

--- views/general_view.py
+++ views/general_view.py
@@ -2,8 +2,10 @@ import streamlit as st
 import pandas as pd
 import os
 import random
-from Pricing_and_Tracking.py import treating_values
+from Pricing_and_Tracking import treating_values
 
-treating_values()
+# Chame a função que trata os valores. Certifique-se de que essa função retorna ou manipula new_df de forma correta.
+new_df = treating_values()  # Supondo que treating_values retorna um DataFrame
 
+# Exibe o DataFrame no Streamlit
 st.dataframe(new_df)
