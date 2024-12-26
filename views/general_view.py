34dfbc0108b11cef2ab32fd2cecdf116eff5a947
Devthalesant <tha.santoro2000@gmail.com>
2024-12-26 20:41:15 +0000

    Updating files

--- views/general_view.py
+++ views/general_view.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import os
 import random
-from Pricing & Tracking import treating_values
+from Pricing&Tracking.ipynb import treating_values
 
 treating_values()
 
