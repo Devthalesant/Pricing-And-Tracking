8a028e9c0e7421f96a4c7419b9cbafd3deb025e6
Devthalesant <tha.santoro2000@gmail.com>
2024-12-26 21:01:30 +0000

    Updating files

--- views/general_view.py
+++ views/general_view.py
@@ -3,8 +3,8 @@ import streamlit as st
 import pandas as pd
 
 # Chame a função que trata os valores. Certifique-se de que essa função retorna ou manipula new_df de forma correta.
-get_dataframe_from_mongodb(collection_name, database_name, query={})
-extract_quote_items(row)
+get_dataframe_from_mongodb()
+extract_quote_items()
 new_df = treating_values()  # Supondo que treating_values retorna um DataFrame
 
 # Exibe o DataFrame no Streamlit
