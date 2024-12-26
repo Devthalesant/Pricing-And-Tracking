784b2bc7cba9d24b01c72aaaca2272720bfbf313
Devthalesant <tha.santoro2000@gmail.com>
2024-12-26 20:50:35 +0000

    Updating files

--- views/general_view.py
+++ views/general_view.py
@@ -5,7 +5,7 @@ import random
 import sys
 
 # Adicione o caminho do seu arquivo ao sys.path
-sys.path.append('/content/drive/MyDrive/python - Thales/P&T/Pricing_and_Tracking.py')
+sys.path.append('/content/drive/MyDrive/python - Thales/P&T/Pricing_and_Tracking')
 from Pricing_and_Tracking import treating_values
 
 # Chame a função que trata os valores. Certifique-se de que essa função retorna ou manipula new_df de forma correta.
