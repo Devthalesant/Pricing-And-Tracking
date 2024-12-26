5d4617d950d257b5f2743f0e3d14e339265a58b6
Devthalesant <tha.santoro2000@gmail.com>
2024-12-26 20:58:04 +0000

    Updating files

--- views/general_view.py
+++ views/general_view.py
@@ -1,12 +1,5 @@
 import streamlit as st
 import pandas as pd
-import os
-import random
-import sys
-
-# Adicione o caminho do seu arquivo ao sys.path
-sys.path.append('/content/drive/MyDrive/python - Thales/P&T/Pricing_and_Tracking')
-from Pricing_and_Tracking import treating_values
 
 # Chame a função que trata os valores. Certifique-se de que essa função retorna ou manipula new_df de forma correta.
 new_df = treating_values()  # Supondo que treating_values retorna um DataFrame
