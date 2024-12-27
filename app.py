c5932ec51ec6b576d8552cadcfd81c97fcf76629
Devthalesant <tha.santoro2000@gmail.com>
2024-12-27 18:28:53 +0000

    Updating files

--- app.py
+++ app.py
@@ -7,19 +7,7 @@ page_1 = st.Page(
     icon=":material/thumb_up:"
 )
 
-# --- PAGE SETUP ---
-page_2 = st.Page(
-    "views/appointments_view.py",
-    title="Análises",
-    icon=":material/thumb_up:",
-    default=True,
-)
 
-# --- SUBPAGES SETUP FOR PAGE 1 ---
-subpage_1_1 = st.Page(
-    "views/Sales_categorys_view.py",
-    title="Evolução Gráfica",
-)
 
 # --- NAVIGATION SETUP [WITHOUT SECTIONS] ---
 # pg = st.navigation(pages=[about_page, project_1_page, project_2_page])
@@ -27,8 +15,7 @@ subpage_1_1 = st.Page(
 # --- NAVIGATION SETUP [WITH SECTIONS]---
 pg = st.navigation(
     {
-        "Visão - Vendas": [page_1,subpage_1_1],
-        "Visão - Agendamento": [page_2],
+        "Visão - Vendas": [page_1]
     }
 )
 
