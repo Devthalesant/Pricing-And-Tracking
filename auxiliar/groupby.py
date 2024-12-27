dc0064c8d199ba40d06753d7cc3a8597da70b429
Devthalesant <tha.santoro2000@gmail.com>
2024-12-27 19:06:09 +0000

    Updating files

--- auxiliar/groupby.py
+++ auxiliar/groupby.py
@@ -7,14 +7,14 @@ import plotly.express as px
 def grafico_barras_vendas(new_df): 
   new_df['DATA'] = pd.to_datetime(new_df['DATA'])
   #tirar hora depois
-  new_df['PERIODO'] = new_df['DATA'].dt.to_period('M')
+  new_df['MES/ANO'] = new_df['DATA'].dt.to_period('M')
 
-  barras_df = new_df.groupby(['PERIODO']).agg({'TOTAL LÍQUIDO ITEM': 'sum'})
-  barras_df['PERIODO'] = barras_df['PERIODO'].astype(str)
+  barras_df = new_df.groupby(['MES/ANO']).agg({'TOTAL LÍQUIDO ITEM': 'sum'})
+  barras_df['MES/ANO'] = barras_df['MES/ANO'].astype(str)
 
   fig = px.bar(
             new_df,
-            x='PERIODO',
+            x='MES/ANO',
             y='TOTAL LÍQUIDO ITEM',
             title='GRÁFICO VENDAS GERAL'
         )
