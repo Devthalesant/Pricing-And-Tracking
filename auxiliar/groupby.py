a0225258aaa6c1ec32431c2bec26967e152400da
Devthalesant <tha.santoro2000@gmail.com>
2024-12-27 18:39:37 +0000

    Updating files

---
+++ auxiliar/groupby.py
@@ -0,0 +1,12 @@
+from pymongo import MongoClient, UpdateOne
+import pandas as pd
+import re
+
+
+def grafico_barras_vendas(new_df): 
+  new_df['DATA'] = pd.to_datetime(new_df['DATA'])
+  new_df['PERIODO'] = new_df['DATA'].dt.to_period('M')
+
+  barras_df = new_df.groupby(['PERIODO']).agg({'TOTAL ITEM': 'sum'})
+
+  return barras_df
--- views/general_view.py
+++ views/general_view.py
@@ -1,11 +1,16 @@
 from pymongo import MongoClient, UpdateOne
 from auxiliar.catching_and_treating_values_functions import *
+from auxiliar.groupby import *
 import streamlit as st
 import pandas as pd
 import re
 
+st.title("Visão Geral - Perído")
 # Getting a dataframe of billchaeges
 billcharges_df = get_dataframe_from_mongodb(collection_name="billcharges_db", database_name="dash_midia")
 
 new_df = treating_values(billcharges_df)
-st.dataframe(new_df)
+var = grafico_barras_vendas(new_df)
+
+st.dataframe(var)
+
