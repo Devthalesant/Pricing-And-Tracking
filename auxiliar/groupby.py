505f8ced62b9fe53b24ceed5b0f49262b5eac7f3
Devthalesant <tha.santoro2000@gmail.com>
2024-12-27 18:43:31 +0000

    Updating files

--- auxiliar/groupby.py
+++ auxiliar/groupby.py
@@ -7,6 +7,6 @@ def grafico_barras_vendas(new_df):
   new_df['DATA'] = pd.to_datetime(new_df['DATA'])
   new_df['PERIODO'] = new_df['DATA'].dt.to_period('M')
 
-  barras_df = new_df.groupby(['PERIODO']).agg({'TOTAL ITEM': 'sum'})
+  barras_df = new_df.groupby(['PERIODO']).agg({'TOTAL LÍQUIDO ITEM': 'sum'})
 
   return barras_df
